fd4d46be6a86311e787cfc0b7ddc4ff0cf679755
Romain Jobert <rjobert@student.42barcelona.com>
2023-09-20 22:23:15 +0200

    first exec and process version

---
+++ exec_process/pipes.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipes.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rjobert <rjobert@student.42barcelo>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/20 21:04:39 by rjobert           #+#    #+#             */
+/*   Updated: 2023/09/20 21:04:41 by rjobert          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	redirect_input(int	input_fd)
+{
+	if (dup2(input_fd, STDIN_FILENO) == -1)
+		return ; // later on treat here exit case and clean mem
+}
+
+void	redirect_output(int output_fd)
+{
+	if (dup2(output_fd, STDOUT_FILENO) == -1)
+		return ; // same as above
+}
+
+/*
+function to open the pipes in the parent before dispatching the children
+process. take exec object, use the number of comands (nbr of process), 
+return noting but update the exec object with an array of initialized pipes.
+if n process -> (n - 1) pipes -> (n - 1) * 2 fd pointing to mem buffer and 
+not to open file tble.
+*/
+void	pipefd_calibrate(t_exec **exec)
+{
+	int	*pipes;
+	int	i;
+	int	nb_cmds;
+
+	if (*exec)
+		return;
+	nb_cmds = (*exec)->total_pcs;
+	pipes = malloc(sizeof(int) * (nb_cmds - 1) * 2);
+	if (!pipes)
+		return (NULL);
+	i = 0;
+	while (i < nb_cmds - 1)
+	{
+		if (pipe(pipes + (2 * i) )== -1)
+			exit(EXIT_FAILURE);
+		i++;
+	}
+	(*exec)->pipes = pipes;
+}
+
+/*
+closing all pipes as per their fd stored in exec object.
+if we have n process, we have n - 1 pipes, so we have (n - 1) * 2 nber of fd
+*/
+void	close_all_pipes(t_exec *exec)
+{
+	int	i;
+
+	i = 0;
+	while (i < (exec->total_pcs - 1) * 2)
+	{
+		close(exec->pipes[i]);
+		i++;
+	}
+}
--- minishell.h
+++ minishell.h
@@ -23,6 +23,7 @@
 typedef struct s_proc {
 	char			**arg;
 	int				fd[2];
+	int				position;
 	struct s_cmd	*prev;
 	struct s_cmd	*next;
 }	t_proc;
@@ -34,10 +35,37 @@ typedef struct s_tok {
 	struct s_tok	*next;
 }	t_tok;
 
+typedef struct s_exec {
+	char	**env;
+	int		*pipes;
+	pid_t	*pids;
+	int		total_pcs;
+	char	*path; 
+}	t_exec;
+
+/*toakenisation process*/
 int		minishell(int argc, char **argv);
 t_tok	*ft_lstnew_tok(char *str, int type);
 void	ft_lstadd_back_tok(t_tok **lst, t_tok *new);
 int		create_tokens(t_tok **lst_tok, char *line);
 void	ft_print_list(t_tok *lst_tok);
 
+
+/*exec and process functions*/
+void	pipefd_calibrate(t_exec **exec);
+void	launch_process(t_exec **exec, t_proc **pcs_chain);
+void	command_process(t_proc **pcs_chain, t_exec **exec, int pos);
+void	launch_process(t_exec **exec, t_proc **pcs_chain);
+void	wait_processes(t_exec *exec);
+char	*exec_path(char **all_path, t_proc *exec_trgt);
+void	execve_bash(t_proc **exec_trgt, t_exec **exec);
+
+/*utils for process and env*/
+void	init_exec(t_exec *exec, t_proc *pcs_chain, char **env);
+int		measure_list(t_proc *lst);
+void	redirect_input(int	input_fd);
+void	redirect_output(int output_fd);
+void	close_all_pipes(t_exec *exec);
+
+/* exec total malloc : pids, pipes, valid_path,   */
 #endif
