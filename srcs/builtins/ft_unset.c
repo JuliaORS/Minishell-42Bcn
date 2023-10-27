360df03c4d5636571fb3f4edb13cc766de1b5035
Julia Olle roset <julia.olle.roset@gmail.com>
2023-10-27 13:56:54 +0200

    norm builtins

--- srcs/builtins/ft_unset.c
+++ srcs/builtins/ft_unset.c
@@ -1,18 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   builtin.c                                          :+:      :+:    :+:   */
+/*   ft_unset.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rjobert <rjobert@student.42barcelo>        +#+  +:+       +#+        */
+/*   By: julolle- <julolle-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 12:15:32 by rjobert           #+#    #+#             */
-/*   Updated: 2023/09/25 12:15:35 by rjobert          ###   ########.fr       */
+/*   Updated: 2023/10/27 13:08:12 by julolle-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 int	check_syntax_unset(char *var);
+
 /*
 we look in the environment for a variable to delete
 1. if incorrect syntax, don't do anythong and goes to nex arg
@@ -20,30 +21,30 @@ we look in the environment for a variable to delete
 3. execute the downsiwe_env (copy everything from current env execpt the index
     of target)
 */
-int ft_unset(t_exec *exec, char **arg)
+int	ft_unset(t_exec *exec, char **arg)
 {
 	int	i;
-	int idx;
+	int	idx;
 
 	if (!exec || !arg || !arg[1])
 		return (0);
 	i = 1;
 	while (arg[i])
 	{
-		
 		if (check_syntax_unset(arg[i]))
 		{
 			idx = search_env_var(exec->env, arg[i]);
 			if (idx != -1)
 			{
 				exec->env = downsize_env(exec->env, idx, 0, 0);
-					if (!exec->env)
-						ft_printf(STDERR_FILENO, "minishell: unset: calloc failed\n");
+				if (!exec->env)
+					ft_printf(STDERR_FILENO, \
+						"minishell: unset: calloc failed\n");
 			}
 		}
 		i++;
 	}
-	return (0);	
+	return (0);
 }
 
 /*
@@ -57,7 +58,8 @@ int	check_syntax_unset(char *var)
 		return (0);
 	if (!ft_isalpha(var[0]) && var[0] != '_')
 	{
-		ft_printf(STDERR_FILENO, "minishell: export: `%s': not a valid identifier\n", var);
+		ft_printf(STDERR_FILENO, \
+			"minishell: export: `%s': not a valid identifier\n", var);
 		return (0);
 	}
 	i = -1;
@@ -65,7 +67,8 @@ int	check_syntax_unset(char *var)
 	{
 		if (!ft_isalnum(var[i]) && var[i] != '_')
 		{
-			ft_printf(STDERR_FILENO, "minishell: export: `%s': not a valid identifier\n", var);
+			ft_printf(STDERR_FILENO, \
+				"minishell: export: `%s': not a valid identifier\n", var);
 			return (0);
 		}
 	}
